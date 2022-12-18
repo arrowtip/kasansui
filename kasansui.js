2c3e53f61c873432c59cb16fe06e4abece46457c
Jakob Schmid <jakobschmid25.12@gmail.com>
2022-12-18 16:10:40 +0100

    add dynamic canvas resize

--- kasansui.js
+++ kasansui.js
@@ -1,3 +1,38 @@
-const mainCanvas = document.getElementById("main-canvas")
-mainCanvas.width = window.innerWidth * 0.9
-mainCanvas.height = window.innerHeight * 0.9
+const mainCanvas = document.getElementById("main-canvas");
+
+initialize();
+
+function initialize() {
+    window.addEventListener("resize", resizeCanvasEvt, false);
+    window.mainCanvas = mainCanvas;
+    window.frac = 0.9;
+    resizeCanvas(mainCanvas, 0.9);
+}
+function resizeCanvas(canvas, frac) {
+    canvas.width = window.innerWidth * frac;
+    canvas.height = window.innerHeight * frac;
+}
+
+function resizeCanvasEvt(evt) {
+    resizeCanvas(evt.currentTarget.mainCanvas, evt.currentTarget.frac);
+}
+
+const context = mainCanvas.getContext('2d');
+const particles = new Array(mainCanvas.width)
+    .fill(0)
+    .map(x => new Array(mainCanvas.height)
+    .fill(0));
+
+function draw() {
+    /*
+    mainCanvas.width = window.innerWidth * 0.9;
+    mainCanvas.height = window.innerHeight * 0.9;
+    */
+    context.fillStyle = 'red';
+    context.fillRect(10, 10, 10, 10);
+
+    window.requestAnimationFrame(draw);
+}
+
+window.requestAnimationFrame(draw);
+

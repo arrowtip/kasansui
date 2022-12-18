ce1dcc520a7b45900d16421f9ca267f4458a98b1
Jakob Schmid <jakobschmid25.12@gmail.com>
2022-12-19 00:27:11 +0100

    finally able to set pixels on canvas

--- kasansui.js
+++ kasansui.js
@@ -1,4 +1,5 @@
 const mainCanvas = document.getElementById("main-canvas");
+const context = mainCanvas.getContext('2d');
 
 initialize();
 
@@ -8,6 +9,7 @@ function initialize() {
     window.frac = 0.9;
     resizeCanvas(mainCanvas, 0.9);
 }
+
 function resizeCanvas(canvas, frac) {
     canvas.width = window.innerWidth * frac;
     canvas.height = window.innerHeight * frac;
@@ -17,18 +19,35 @@ function resizeCanvasEvt(evt) {
     resizeCanvas(evt.currentTarget.mainCanvas, evt.currentTarget.frac);
 }
 
-const context = mainCanvas.getContext('2d');
-const particles = new Array(mainCanvas.width)
-    .fill(0)
-    .map(x => new Array(mainCanvas.height)
-    .fill(0));
+mainCanvas.addEventListener('mousemove', mousePos, false);
 
 function draw() {
-    const myImageData = context.createImageData(mainCanvas.width, mainCanvas.height);
     context.fillStyle = 'red';
     context.fillRect(10, 10, 10, 10);
 
+    var imageData = context.getImageData(0, 0, mainCanvas.width, mainCanvas.height);
+    var pixels = imageData.data;
+
+    for (var i = 0; i < pixels.length; i += 4) {
+        pixels[i] = 255;
+        pixels[i + 1] = 0;
+        pixels[i + 2] = 0;
+        pixels[i + 3] = 0;
+    }
+
+    /*context.putImageData(imageData, 0, 0);*/
+
+
+
     window.requestAnimationFrame(draw);
+}
+
+function getMousePos(canvas, event) {
+    let rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
 }
 
 window.requestAnimationFrame(draw);
